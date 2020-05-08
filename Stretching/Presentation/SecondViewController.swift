df732d8167a3b81e496b2106d8ffdb89d6a0d21e
Arif Luthfiansyah <lutluthfi@icloud.com>
2020-05-08 14:18:26 +0700

    Initial project

---
+++ Stretching/Presentation/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Stretching
+//
+//  Created by Arif Luthfiansyah on 08/05/20.
+//  Copyright © 2020 MC2-11. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
