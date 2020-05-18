c91ef629029d9dd15f1322c407656c9037caa18c
Arif Luthfiansyah <lutluthfi@icloud.com>
2020-05-18 14:50:43 +0700

    Add StretchingAssetKit

---
+++ Stretching/Presentation/Utils/StretchingAssetKit.swift
@@ -0,0 +1,44 @@
+//
+//  StretchingAssetKit.swift
+//  Stretching
+//
+//  Created by Arif Luthfiansyah on 18/05/20.
+//  Copyright © 2020 MC2-11. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct StretchingAssetKit {
+    static let imageGuide1 = UIImage(named: "1")!
+    static let imageGuide2 = UIImage(named: "2")!
+    static let imageGuide3 = UIImage(named: "3")!
+    static let imageGuide4 = UIImage(named: "4")!
+    static let imageGuide5 = UIImage(named: "5")!
+    static let imageGuide6 = UIImage(named: "6")!
+    static let imageGuide7 = UIImage(named: "7")!
+    static let imageGuide8 = UIImage(named: "8")!
+    static let imageGuide9 = UIImage(named: "9")!
+    static let imageGuide10 = UIImage(named: "10")!
+    static let imageGuide11 = UIImage(named: "11")!
+    static let imageGuide12 = UIImage(named: "12")!
+    static let imageGuide13 = UIImage(named: "13")!
+    static let imageGuide14 = UIImage(named: "14")!
+    static let imageGuide15 = UIImage(named: "15")!
+    static let imageGuide16 = UIImage(named: "16")!
+    static let imageGuide17 = UIImage(named: "17")!
+    static let imageGuide18 = UIImage(named: "18")!
+    static let imageGuide19 = UIImage(named: "19")!
+    static let imageGuide20 = UIImage(named: "20")!
+    static let imageGuide21 = UIImage(named: "21")!
+    static let imageGuide22 = UIImage(named: "22")!
+    static let imageGuide23 = UIImage(named: "23")!
+    static let imageGuide24 = UIImage(named: "24")!
+    static let imageGuide25 = UIImage(named: "25")!
+    static let imageGuide26 = UIImage(named: "26")!
+    static let imageGuide27 = UIImage(named: "27")!
+    static let imageGuide28 = UIImage(named: "28")!
+    static let imageGuide29 = UIImage(named: "29")!
+    static let imageGuide30 = UIImage(named: "30")!
+    static let imageGuide31 = UIImage(named: "31")!
+}
