12eead60af2aef90d9f31164695fb802a60e4487
Arif Luthfiansyah <lutluthfi@icloud.com>
2020-05-26 19:59:56 +0700

    Initial RewardScene

---
+++ Stretching/Presentation/RewardScene/RewardViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RewardViewController.swift
+//  Stretching
+//
+//  Created by Arif Luthfiansyah on 26/05/20.
+//  Copyright © 2020 MC2-11. All rights reserved.
+//
+
+import UIKit
+
+class RewardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
